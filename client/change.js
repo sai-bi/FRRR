d1b09325f9cc538de67e6af76cc7221b395650cb
毕赛 <soundsilencebisai@gmail.com>
2013-08-24 00:15:06 +0800

    Finish sending URL

--- client/change.js
+++ client/change.js
@@ -1,37 +1,23 @@
 function checkForValidUrl(tabId, changeInfo, tab) {
 //if we could find "photo" in the url, we will process this page;
+  var url = tab.url;
+
+  //The repition problem for URL should be considered when receivng data.
+  //++++++++
+
   if (tab.url.indexOf("photo") > -1 && tab.url.indexOf("renren") > -1) {
     // ... show the page action.
     chrome.pageAction.show(tabId);
   }
   else
   	return;
-  chrome.tabs.executeScript(null, {file: "extractURLBack.js"});
+  if(url != oldURL){
+     chrome.tabs.executeScript(null, {file: "extractURL.js"});
+     oldURL = url;
+  }
 };
 
 // Listen for any changes to the URL of any tab.
+var oldURL = "wtf";
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
-window.onload = function(){
-	// Handle the data response
-	var handleDataEvent = function(d) {
-	  var data = chrome.socket.read(d.socketId);
-	  console.log(data);
-	};
-
-	// Create the Socket
-	chrome.socket.create('udp', '127.0.0.1', 54125, { onEvent: handleDataEvent },
-	 function(socketInfo) {
-	   // The socket is created, now we want to connect to the service
-	   var socketId = socketInfo.socketId;
-	   var arrayBuffer = new ArrayBuffer(10000);
-	   chrome.socket.connect(socketId, function(result) {
-	     // We are now connected to the socket so send it some data
-	     chrome.socket.write(socketId, arrayBuffer,
-	       function(sendInfo) {
-	         console.log("wrote " + sendInfo.bytesWritten);
-	       }
-	     );
-	   });
-	 }
-	);
-};
+chrome.tabs.onCreated.addListener(checkForValidUrl);
