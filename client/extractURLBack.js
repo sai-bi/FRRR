05cca64c1676d972cf5d2e0b3cd22898eca60655
毕赛 <soundsilencebisai@gmail.com>
2013-08-24 20:20:50 +0800

    cookie expiration check

--- client/extractURLBack.js
+++ client/extractURLBack.js
@@ -1,15 +1,9 @@
-/*
-var photoContainer = document.getElementById('photoContainer');
-
-if(photoContainer == null)
-	return;
-
-var anchorA = photoContainer.childNodes[1]; 
-
-var img = anchorA.childNodes[0];
-
-var url = img.getAttribute('src'); //url stores the link to the image we want
-*/
-//Following should I to sent url to 'Backgournd Page' or 'Server'?
-//I am not sure.
-//alert(url);
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                 "from the extension");
+//    var cookie = request.greeting;
+//    alert(cookie);
+//       sendResponse({farewell: "goodbye"});
+// });
