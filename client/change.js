d071b9768cdc9f3bb307e92dfb4d0e16fca19d0c
毕赛 <soundsilencebisai@gmail.com>
2013-08-29 00:42:21 +0800

    Face detector is working now

--- client/change.js
+++ client/change.js
@@ -5,7 +5,7 @@ function checkForValidUrl(tabId, changeInfo, tab){
   //avoid sending repetive urls to our server.
   var url = tab.url;
 
-  if (tab.url.indexOf("photo.renren") > -1) {
+  if (tab.url.indexOf("renren.com") > -1) {
     // ... show the page action.
     chrome.pageAction.show(tabId);
   }
@@ -13,11 +13,14 @@ function checkForValidUrl(tabId, changeInfo, tab){
   	return;
   if(url != oldURL){
   	 oldURL = url;
-     chrome.tabs.executeScript(null, {file: "sendCookie.js"});
+      
+     //chrome.tabs.executeScript(null, {file: "sendCookie.js"});
      chrome.tabs.executeScript(null, {file: "extractURL.js"});
+     //chrome.tabs.executeScript(null, {file: "drawBox.js"});
   }
 };
 
+//Boolean reload = false;
 // Listen for any changes to the URL of any tab.
 var oldURL = "wtf";
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
