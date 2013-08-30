787869b5cb20a5ca2eabd8978fd0d79505a2a707
Zhiyuan Shi <zhiyuanshi@server.fake>
2013-08-31 05:32:07 +0800

    download is working

--- renren/src/download.rb
+++ renren/src/download.rb
@@ -5,51 +5,62 @@ require 'mysql2'
 require 'mechanize'
 require_relative 'renren'
 
+def clear_print s
+  print "\r\033[K"
+  print s
+end
+
 module Download
 
   module_function
 
   def download conn, path
-    puts "Downloading photos\n"
+    clear_print "Downloading photos\n"
 
     conn.query 'use renren'
     agent = Mechanize.new
 
-    puts '  Retrieving photos'
+    clear_print '  Retrieving photos'
 
     photos_count = (conn.query 'select count(*) from photos').first['count(*)']
     success_count = 0
-    failed_count = 0
+    failure_count = 0
 
-    puts "  Retrieved #{photos_count} photos\n"
+    clear_print "  Retrieved #{photos_count} photos\n"
     photos_to_download = conn.query 'select * from photos where downloaded = false'
     photos_to_download_count = (conn.query 'select count(*) from photos where downloaded = false').first['count(*)']
-    puts "  #{photos_to_download_count} to download"
+    clear_print "  #{photos_to_download_count} to download\n"
 
     FileUtils.cd path do
 
-      photos_to_download.each do |photo|
-        #puts "  #{i} of #{photos_count} [Downloading photos]"
-        #puts photo['caption']
+      photos_to_download.each_with_index do |photo, i|
+        clear_print "  #{i} of #{photos_to_download_count} (#{failure_count} failed) [Downloading] #{photo['url']}"
+        download_success = false
         urls = Renren::make_backup_urls photo['url']
+        FileUtils.mkdir_p Renren::make_album_path photo['user_id'], photo['album_id']
         urls.each do |url|
           begin
-            #agent.download url, (make_photo_path user_id, album_id, photo_id)
-          rescue Mechanize::ResponseCodeError, Mechanize::ResponseReadError, Net::HTTP::Persistent::Error, StandardError
-            failed_count += 1
+            agent.download url, (Renren::make_photo_path photo['user_id'], photo['album_id'], photo['photo_id'])
+          rescue Mechanize::ResponseCodeError, Mechanize::ResponseReadError, Net::HTTP::Persistent::Error
             next
           else
-            success_count += 1
-            url = conn.escape url
-            #conn.query "update photos set downloaded = true where url = #{url}"
+            download_success = true
+            conn.query \
+              "update photos set downloaded = true
+               where user_id = #{photo['user_id']} and album_id = #{photo['album_id']} and photo_id = #{photo['photo_id']}"
             break
           end
+        end
+        if download_success
+          success_count += 1
+        else
+          failure_count += 1
         end
       end
 
     end
 
-    puts "  #{success_count} photos downloaded, #{failed_count} failed\n"
+    clear_print "  #{success_count} photos downloaded, #{failure_count} failed\n"
 
   end
 end
